4db1630685bc605b8bd350ac356e177d03815346
Kenneth Aalberg <kenneaal@gmail.com>
2020-06-07 22:52:12 +0200

    Oops!

--- FCMS/views/login.py
+++ FCMS/views/login.py
@@ -48,15 +48,19 @@ def register_view(request):
         if res:
             return {'reg_failure': True, 'message': 'User exists!'}
         cryptpass = pwd_context.hash(request.params['pass'])
-        newuser = user.User(username=request.params['email'], password=cryptpass, userlevel=1, cmdr_name=request.params['cmdr_name'], has_validated=False, public_carrier=True, banned=False)
+        newuser = user.User(username=request.params['email'], password=cryptpass, userlevel=1,
+                            cmdr_name=request.params['cmdr_name'], has_validated=False, public_carrier=True,
+                            banned=False)
         request.dbsession.add(newuser)
         return exc.HTTPFound(location=request.route_url('login'))
     return {'project': 'Fleet Carrier Management System'}
 
 
 @view_config(route_name='oauth', renderer='../templates/register.jinja2')
-def oauth(request):
-    return exc.HTTPFound(location=capi.get_auth_url())
+def oauth_view(request):
+    print("In Oauth request.")
+    url, state = capi.get_auth_url()
+    return exc.HTTPFound(location=url)
 
 
 @view_config(route_name='oauth_callback', renderer='../templates/register.jinja2')
@@ -83,28 +87,34 @@ def oauth_finalize(request):
         jcarrier = capi.get_carrier(user)
         print(jcarrier)
         services = jcarrier['market']['services']
-        oc = request.dbsession.query(carrier.Carrier).filter(carrier.Carrier.callsign == jcarrier['name']['callsign']).one_or_none()
+        oc = request.dbsession.query(carrier.Carrier).filter(
+            carrier.Carrier.callsign == jcarrier['name']['callsign']).one_or_none()
         if oc:
             print("Already have their carrier! No need for further shenanigans.")
             return {'project': 'Got a callback, updated existing user oauth keys.',
                     'meta': {'refresh': True, 'target': '/my_carrier', 'delay': 5}}
-        newcarrier = carrier.Carrier(owner=user.id, callsign=jcarrier['name']['callsign'], name=jcarrier['name']['vanityName'],
+        newcarrier = carrier.Carrier(owner=user.id, callsign=jcarrier['name']['callsign'],
+                                     name=jcarrier['name']['vanityName'],
                                      currentStarSystem=jcarrier['currentStarSystem'], balance=jcarrier['balance'],
                                      fuel=jcarrier['fuel'], state=jcarrier['state'], theme=jcarrier['theme'],
-                                     dockingAccess=jcarrier['dockingAccess'], notoriousAccess=jcarrier['notoriousAccess'],
-                                     totalDistanceJumped=jcarrier['itinerary']['totalDistanceJumpedLY'], currentJump=jcarrier['itinerary']['currentJump'],
-                                     taxation=jcarrier['finance']['taxation'], coreCost=jcarrier['finance']['coreCost'], servicesCost=jcarrier['finance']['servicesCost'],
-                                     jumpsCost=jcarrier['finance']['jumpsCost'], numJumps=jcarrier['finance']['numJumps'], hasCommodities=True,
-                                     hasCarrierFuel=True, hasRearm=True if services['rearm']=='ok' else False,
-                                     hasShipyard=True if services['shipyard']=='ok' else False,
-                                     hasOutfitting=True if services['outfitting']=='ok' else False,
-                                     hasBlackMarket=True if services['blackmarket']=='ok' else False,
-                                     hasVoucherRedemption=True if services['voucherredemption']=='ok' else False,
-                                     hasExploration=True if services['exploration']=='ok' else False,
+                                     dockingAccess=jcarrier['dockingAccess'],
+                                     notoriousAccess=jcarrier['notoriousAccess'],
+                                     totalDistanceJumped=jcarrier['itinerary']['totalDistanceJumpedLY'],
+                                     currentJump=jcarrier['itinerary']['currentJump'],
+                                     taxation=jcarrier['finance']['taxation'], coreCost=jcarrier['finance']['coreCost'],
+                                     servicesCost=jcarrier['finance']['servicesCost'],
+                                     jumpsCost=jcarrier['finance']['jumpsCost'],
+                                     numJumps=jcarrier['finance']['numJumps'], hasCommodities=True,
+                                     hasCarrierFuel=True, hasRearm=True if services['rearm'] == 'ok' else False,
+                                     hasShipyard=True if services['shipyard'] == 'ok' else False,
+                                     hasOutfitting=True if services['outfitting'] == 'ok' else False,
+                                     hasBlackMarket=True if services['blackmarket'] == 'ok' else False,
+                                     hasVoucherRedemption=True if services['voucherredemption'] == 'ok' else False,
+                                     hasExploration=True if services['exploration'] == 'ok' else False,
                                      lastUpdated=datetime.now())
         request.dbsession.add(newcarrier)
-        #TODO: Inject rest of the data too.
-        #TODO: Redirect to my_carrier after delay.
+        # TODO: Inject rest of the data too.
+        # TODO: Redirect to my_carrier after delay.
     except:
         return {'project': 'Failed to retrieve your carrier. Did you buy one?'}
     return {'project': 'OAuth flow completed. Carrier added.',
