4ac3f0ed005ca7623827a27049179673ddd7c082
Kenneth Aalberg <kenneaal@gmail.com>
2020-06-10 14:59:56 +0200

    Extra hot hotfix.

--- FCMS/utils/carrier_data.py
+++ FCMS/utils/carrier_data.py
@@ -289,7 +289,7 @@ def update_carrier(request, cid, user):
         mycarrier.hasExploration = True if services['exploration'] == 'ok' else False
         mycarrier.hasRepair = True if services['repair'] == 'ok' else False
         mycarrier.hasRefuel = True if services['refuel'] == 'ok' else False
-        if not coords['error']:
+        if 'error' in coords:
             mycarrier.x = coords['x']
             mycarrier.y = coords['y']
             mycarrier.z = coords['z']
