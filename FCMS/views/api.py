4f9a86606bd22dd696f3f868a056c7c93b3d0dad
Kenneth Aalberg <kenneaal@gmail.com>
2020-06-11 17:52:48 +0200

    Let's not leave the debugging GIF there...

--- FCMS/views/api.py
+++ FCMS/views/api.py
@@ -53,7 +53,7 @@ def api_view(request):
                 for hook in hooks:
                     log.debug(f"Process hook {hook['webhook_url']}")
                     if hook['webhook_type'] == 'discord':
-                        res = webhooks.cancel_jump(request, mycarrier.id, hook['webhook_url'], True)
+                        res = webhooks.cancel_jump(request, mycarrier.id, hook['webhook_url'])
                         log.debug(f"Hook result: {res}")
 
     return {'Status': 'Maybe OK?'}
