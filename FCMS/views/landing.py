1119de7405243a3927e6504f2f91fb0ac904f330
Kenneth Aalberg <kenneaal@gmail.com>
2020-06-23 17:44:30 +0200

    Prep landing page.

---
+++ FCMS/views/landing.py
@@ -0,0 +1,32 @@
+import colander
+from deform import widget, Form
+from pyramid.view import view_config
+
+from ..models import Carrier
+
+
+class Search(colander.MappingSchema):
+    system = colander.SchemaNode(colander.String(),
+                                 widget=widget.AutocompleteInputWidget(
+                                     values='https://system.api.fuelrats.com/typeahead', min_length=3))
+
+
+@view_config(route_name='landing', renderer='../templates/landing.jinja2')
+def my_view(request):
+    request.response.headers.update({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Max-Age': '1728000',
+    })
+
+    ct =request.dbsession.query(Carrier).filter(Carrier.trackedOnly==True)
+    cr = request.dbsession.query(Carrier).filter(Carrier.trackedOnly==False)
+    print(ct.count())
+    print(cr.count())
+    schema = Search()
+    searchform = Form(schema, action='/search/system', buttons=('submit',), method='POST')
+    rendered_form = searchform.render()
+    return {'searchform': rendered_form,  'deform': True, 'tracked_carriers': ct.count(),
+            'registered_carriers': cr.count()}
